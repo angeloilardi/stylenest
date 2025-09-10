4eac834277e69543d21f74f0d501ca425a0f8cfa
Angelo Ilardi <ange448@gmail.com>
2025-09-10 22:25:04 +0100

    section added - footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,81 @@
+import categories from "./../data/categories.json";
+import collections from "./../data/collections.json";
+import {
+  RiInstagramLine,
+  RiFacebookBoxLine,
+  RiTwitterXFill,
+  RiGithubLine,
+  RiYoutubeLine,
+} from "react-icons/ri";
+
 const Footer = () => {
   return (
-    <footer className="mt-40">
-      <div className="flex">
-        <h2></h2>
-        <p></p>
-        <form action="" className="flex flex-col gap-4 w-full">
-          <label htmlFor="email" className="sr-only" />
-          <input
-            type="text"
-            id="email"
-            placeholder="Enter your email"
-            className="w-full border border-neutral-200 bg-neutral-200/50 p-2"
-          />
-          <button className="w-full bg-blue-500 text-white p-2">
-            Subscribe
-          </button>
-        </form>
-      </div>
-    </footer>
+    <>
+      <footer className="mt-40">
+        <div className="border-b border-neutral-200">
+          <div className="flex flex-col lg:flex-row md:justify-between lg:items-center">
+            <div>
+              <h3 className="text-xl font-semibold mb-4">
+                Join our newsletter
+              </h3>
+              <p className="mb-8">
+                We'll send you a nice letter once per week. No spam.
+              </p>
+            </div>
+            <form
+              action=""
+              className="flex flex-col gap-4 w-full mb-10 md:flex-row md:h-12 lg:w-120 md:items-center md:mb-0"
+            >
+              <label htmlFor="email" className="sr-only" />
+              <input
+                type="text"
+                id="email"
+                placeholder="Enter your email"
+                className="w-full border border-neutral-200 bg-neutral-200/50 p-2 rounded-md"
+              />
+              <button className="w-full bg-blue-500 text-white p-2 md:w-36">
+                Subscribe
+              </button>
+            </form>
+          </div>
+          <div className="lg:flex lg:flex-row lg:gap-52 md:mt-12">
+            <div>
+              <img src="public/logo.svg" />
+              <p className="my-8">
+                Craft stunning style journeys that weave more joy into every
+                thread.
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row md:gap-36">
+              <div className="flex flex-col gap-2 mb-6">
+                <span className="mb-4 text-neutral-400"> SHOP CATEGORIES</span>
+                <ul className="flex flex-col gap-2">
+                  {categories.map((category) => {
+                    return <li>{category.name}</li>;
+                  })}
+                </ul>
+              </div>
+              <div className="flex flex-col gap-2 mb-6">
+                <span className="mb-4 text-neutral-400"> SHOP COLLECTIONS</span>
+                <ul className="flex flex-col gap-2">
+                  {collections.map((collection) => {
+                    return <li>{collection.name}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="py-10">© 2024 StyleNest, Inc. All rights reserved.</p>
+        <div className="flex gap-4">
+          <RiInstagramLine size={24} />
+          <RiFacebookBoxLine size={24} />
+          <RiTwitterXFill size={24} />
+          <RiGithubLine size={24} />
+          <RiYoutubeLine size={24} />
+        </div>
+      </footer>
+    </>
   );
 };
 
